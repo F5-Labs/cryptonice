97b8b8c8cfb56d16322bbbe318b15951a95342d3
knewbold17 <katiesnewbold@gmail.com>
2020-07-06 18:34:55 -0400

    catches malformed input

--- sample_script.py
+++ sample_script.py
@@ -2,6 +2,7 @@ from cryptonice import scanner
 import argparse
 import json
 
+
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("input_file", help="JSON input file of scan commands")
@@ -13,7 +14,10 @@ def main():
 
     output_data, hostname = scanner.scanner_driver(input_data)
 
-    scanner.writeToJSONFile(hostname, output_data)
+    if output_data is not None and hostname is not None:
+        scanner.writeToJSONFile(hostname, output_data)
+    else:
+        print("Error with input - scan was not completed")
 
 
 if __name__ == "__main__":
