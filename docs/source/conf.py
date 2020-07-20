a0f545dded557bd0fbf774e993bb8bb0a56cfb56
David Warburton <d.warburton@f5.com>
2020-07-20 11:10:34 +0100

    Docs update

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -77,11 +77,12 @@ pygments_style = None
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-import sphinx_glpi_theme
 
-html_theme = "glpi"
+#import sphinx_glpi_theme
+#html_theme = "glpi"
+#html_theme_path = sphinx_glpi_theme.get_html_themes_path()
 
-html_theme_path = sphinx_glpi_theme.get_html_themes_path()
+html_theme = 'default'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
