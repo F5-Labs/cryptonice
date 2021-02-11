10d06051c70a5d43d89022e6d883b58cd31ef72e
warburtr0n <d.warburton@f5.com>
2021-02-11 12:38:15 +0000

    Moved modules in to main cryptonice folder. Changed reqs in setup.py

--- setup.py
+++ setup.py
@@ -54,7 +54,7 @@ setup(
     install_requires=["sslyze>=4.0.0",
                       "dnspython>=2.0.0",
                       "http-client>=0.1.21",
-                      "urllib3>=1.25.9",
+                      "urllib3~=1.22",
                       "ipaddress>=1.0.22",
                       "pathlib~=1.0.1",
                       "bs4>=0.0.1",
