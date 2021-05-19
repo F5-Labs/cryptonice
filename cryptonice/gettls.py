ede1f6b7fbfaa36e6c515892c4f1f5ce2a506767
warburtr0n <d.warburton@f5.com>
2021-05-19 10:49:31 +0100

    Import hashes primitives from cryptography

--- cryptonice/gettls.py
+++ cryptonice/gettls.py
@@ -12,6 +12,7 @@ from sslyze.errors import ConnectionToServerFailed
 
 from cryptography import x509
 from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.asymmetric import rsa, dsa, ec, ed448, ed25519
 from datetime import datetime
 from typing import List, cast
